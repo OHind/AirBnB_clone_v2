457ff5a4b4622c5aaad8271807d04074be40f607
sharonkisenga <sharonglor69@mail.com>
2023-11-08 03:43:43 -0800

    clone

---
+++ 100-clean_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# 
+import os
+from fabric.api import *
+
+env.hosts = ["100.25.23.209","52.91.119.65"]
+
+
+def do_clean(number=0):
+    """Do the clean"""
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-AirBnB_clone_v2 project done by Hind and Sharon
+AIRBNB CLONE V2	AirBnB_clone_v2 project done by Hind and Sharon
