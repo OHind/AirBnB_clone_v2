db08e1d48ba694e5f2b136f5b7b2297dab1c48f5
sharonkisenga <sharonglor69@gmail.com>
2023-11-24 00:51:52 -0800

    task2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
+"""script that starts a Flask web application"""
+
 from flask import Flask
-"""web Flasks"""
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """web flask"""
-    return "Hello HBNB!"
+@app.route('/')
+def hello_world():
+    """script that starts a Flask web application"""
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """web flask"""
-    return "HBNB"
+@app.route('/hbnb')
+def hello():
+    """script that starts a Flask web application"""
+    return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def display_text(text):
-    """web flask"""
-    return 'C %s' % text.replace('_', ' ')
+@app.route('/c/<text>')
+def c_text(text):
+    """script that starts a Flask web application"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
